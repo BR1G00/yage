d0c8277e7c12585a71223806cf41e0eb36470561
Alessandro Decarli <adecarli29@gmail.com>
2025-10-10 14:53:09 +0200

    feat: added resizeable sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import "./App.css";
 import { Graph } from "./components/Graph";
 import { NodeSidebar } from "./components/NodeSidebar";
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "./components/ui/resizable";
 import { SidebarProvider } from "./components/ui/sidebar";
 
 function App() {
   return (
-    <SidebarProvider>
-      <Graph />
-      <NodeSidebar></NodeSidebar>
-    </SidebarProvider>
+    <ResizablePanelGroup direction="horizontal">
+      <ResizablePanel defaultSize={75} minSize={30} maxSize={80}>
+        <Graph />
+      </ResizablePanel>
+      <ResizableHandle withHandle />
+      <ResizablePanel defaultSize={25} minSize={20} maxSize={50}>
+        <SidebarProvider>
+          <NodeSidebar></NodeSidebar>
+        </SidebarProvider>
+      </ResizablePanel>
+    </ResizablePanelGroup>
   );
 }
 
