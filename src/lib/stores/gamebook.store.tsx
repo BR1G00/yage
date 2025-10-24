34f87ae64d0cdf1c8ba3c1684ff87fbba454133d
Nicolò Festa <nicolof@monade.io>
2025-10-24 16:32:07 +0200

    fix: create only the first page for new project

--- src/lib/stores/gamebook.store.tsx
+++ src/lib/stores/gamebook.store.tsx
@@ -22,66 +22,14 @@ const initialNodes: Node[] = [
     data: {
       type: "start",
       title: "Page 1",
-      content: "Starting Page!!!!!",
-    },
-    type: "page",
-    ...nodeDefaults,
-  },
-  {
-    id: "n2",
-    position: { x: 400, y: 400 },
-    data: {
-      title: "Choice 1",
-      content: "This is the content of choice 1",
-    },
-    type: "choice",
-    ...nodeDefaults,
-  },
-  {
-    id: "n3",
-    position: { x: 400, y: 800 },
-    data: { title: "Choice 2", content: "This is the content of choice 2" },
-    type: "choice",
-    ...nodeDefaults,
-  },
-  {
-    id: "n4",
-    position: { x: 800, y: 400 },
-    data: { type: "end", title: "Page 2", content: "Game Over" },
-    type: "page",
-    ...nodeDefaults,
-  },
-  {
-    id: "n5",
-    position: { x: 800, y: 800 },
-    data: {
-      type: "normal",
-      title: "Page 3",
-      content: "This is the content of page 3",
+      content: "",
     },
     type: "page",
     ...nodeDefaults,
   },
 ];
 
-const initialEdges: Edge[] = [
-  {
-    id: "n1-n2",
-    source: "n1",
-    target: "n2",
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-  {
-    id: "n2-n4",
-    source: "n2",
-    target: "n4",
-    markerEnd: {
-      type: MarkerType.Arrow,
-    },
-  },
-];
+const initialEdges: Edge[] = [];
 
 const useGamebookStore = create<GamebookStore>()(
   persist(
