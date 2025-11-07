1724f8662fb452ab038fd9b21e80f9b87101cc8a
Nicolò Festa <nicolof@monade.io>
2025-11-07 14:58:49 +0100

    use Sonner Toast instead of alert

--- src/hooks/use-graph-manager.ts
+++ src/hooks/use-graph-manager.ts
@@ -1,48 +1,50 @@
 import useGamebookStore from "@/lib/stores/gamebook.store";
 import type { Edge, Node } from "@xyflow/react";
 import { useCallback, useEffect } from "react";
+import { toast } from "sonner";
 
 export const useGraphManager = () => {
-    const nodes = useGamebookStore((state) => state.nodes);
-    const edges = useGamebookStore((state) => state.edges);
-    const setNodes = useGamebookStore((state) => state.setNodes);
-    const setEdges = useGamebookStore((state) => state.setEdges);
+  const nodes = useGamebookStore((state) => state.nodes);
+  const edges = useGamebookStore((state) => state.edges);
+  const setNodes = useGamebookStore((state) => state.setNodes);
+  const setEdges = useGamebookStore((state) => state.setEdges);
 
-    const handleOpen = useCallback(
-        (data: unknown) => {
-            const { nodes, edges } = data as { nodes: Node[]; edges: Edge[] };
-            setNodes(nodes);
-            setEdges(edges);
-        },
-        [setNodes, setEdges]
-    );
+  const handleOpen = useCallback(
+    (data: unknown) => {
+      const { nodes, edges } = data as { nodes: Node[]; edges: Edge[] };
+      setNodes(nodes);
+      setEdges(edges);
+    },
+    [setNodes, setEdges]
+  );
 
-    const handleSave = useCallback(() => {
-        const blob = new Blob([JSON.stringify({ nodes, edges })], {
-            type: "application/json",
-        });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = "gamebook.json";
-        a.click();
-        URL.revokeObjectURL(url);
-    }, [nodes, edges]);
+  const handleSave = useCallback(() => {
+    const blob = new Blob([JSON.stringify({ nodes, edges })], {
+      type: "application/json",
+    });
+    const url = URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = "gamebook.json";
+    a.click();
+    URL.revokeObjectURL(url);
+  }, [nodes, edges]);
 
-    useEffect(() => {
-        const cleanup = window?.electronAPI?.onOpen((data) => {
-            handleOpen(data);
-        });
+  useEffect(() => {
+    const cleanup = window?.electronAPI?.onOpen((data) => {
+      handleOpen(data);
+    });
 
-        return cleanup;
-    }, [handleOpen]);
+    return cleanup;
+  }, [handleOpen]);
 
   useEffect(() => {
     const cleanup = window?.electronAPI?.onSave(() => {
       try {
         handleSave();
       } catch (error) {
-        window.alert("Errore durante il salvataggio del file");
+        toast.error("Failed to save file");
+        console.error("Failed to save file", error);
       }
     });
 
