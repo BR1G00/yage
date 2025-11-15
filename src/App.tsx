a1db27ff57d5392646b35f53dd1d8716ed162357
Nicolò Festa <nicolof@monade.io>
2025-11-15 09:51:57 +0100

    show current file path

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import { NodeSidebar } from "./components/NodeSidebar";
 import { SidebarInset, SidebarProvider } from "./components/ui/sidebar";
 import { useGraphManager } from "./hooks/use-graph-manager";
 import { Toaster } from "sonner";
+import useGamebookStore from "./lib/stores/gamebook.store";
 
 function App() {
   useGraphManager();
+  const currentFilePath = useGamebookStore((state) => state.currentFilePath);
 
   return (
     <SidebarProvider>
+      <p>{currentFilePath}</p>
       <SidebarInset>
         <Graph />
       </SidebarInset>
